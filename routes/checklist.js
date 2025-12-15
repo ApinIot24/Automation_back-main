c3fe060dd4bed307a1c954bfd658fa02d6b0dd7c
Muhamad Dwi Cahyo <100329714+dwicahyo1512@users.noreply.github.com>
2025-12-15 15:14:15 +0700

    update: checklist

--- routes/checklist.js
+++ routes/checklist.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getChecklistByLineWeek, getChecklistByQRCode, updateChecklist } from "../controllers/ChecklistController";
+import { getChecklistByLineWeek, getChecklistByQRCode, updateChecklist } from "../controllers/ChecklistController.js";
 
 const app = Router();
 app.get("/mobile/current/week/:line/:currentweek", getChecklistByLineWeek);
